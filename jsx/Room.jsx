fe890a142af1c1d623c7895db06ef93790660059
Nishida <nishida@zyyx.jp>
2015-08-10 13:43:48 +0900

    Socket.ioつなぐとこまで

--- jsx/Room.jsx
+++ jsx/Room.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 
 var Room = React.createClass({
+  componentDidMount: function() {
+    var that = this;
+    this.socket = io();
+    this.socket.on('message', function(message){
+      this.setState({message: message})
+    });
+    this.socket.emit('Entered into Room');
+  },
+
   render: function() {
     return (
       <div class="room">
         <div id="campass">
+          <textarea>aaa</textarea>
         </div>
         <div id="members">
         </div>
--- server.js
+++ server.js
@@ -3,4 +3,15 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3123);
+var server = app.listen(3123);
+
+var io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  console.log('Connected');
+  socket.on('message', function(message){
+    console.log(message);
+  });
+});
+
+console.log('server started');
